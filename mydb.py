6dfc9ce24250a4f063feeccc0ea17d836c7e0d20
bhavy9590 <bhavypatel9590@gmail.com>
2024-11-22 00:46:17 +0530

    Initial commit

---
+++ mydb.py
@@ -0,0 +1,21 @@
+# #pip install mysql
+# #pip install mysql-connector
+# #pip install mysql-connector-python
+
+
+# import mysql.connector
+
+# database = mysql.connector.connect(
+#         host = 'localhost',
+#         user = 'root',
+#         passwd = 'password123'
+# )
+
+# cursorobject = database.cursor()
+
+# #create a database
+
+# cursorobject.execute("CREATE DATABASE stegowl")
+
+# print("Allo Done! ")
+
